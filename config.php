49eaea68e71868289d6a403a0163d5fd7838d590
manideepak <kanugalamanideepak@gmail.com>
2025-10-21 20:27:37 +0530

    Save changes: update config/create/edit/index/login + DB and remove shortcut

--- config.php
+++ config.php
@@ -1,12 +1,12 @@
+
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "blog";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+$host = '127.0.0.1';
+$user = 'root';
+$pass = '';
+$db   = 'your_database_name';
 
+$conn = new mysqli($host, $user, $pass, $db);
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    die('Connect error: ' . $conn->connect_error);
 }
-?>
